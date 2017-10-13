a885e11f8be56beb9d0aca35ce3aca027ba4123c
Glen Maddern <glenmaddern@gmail.com>
2017-10-13 15:35:58 +1100

    async example

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,11 @@
 import store from './store'
 
 const MY_AVATAR = 'http://fillmurray.com/106/106'
-export const addMessageToStore = (line) => {
+export const addMessageToStore = async (line) => {
   const lastMessage = store.messages[store.messages.length - 1]
+
+  store.isSyncing = true
+  await fetch('/api/new_message', { body: line })
 
   if (lastMessage && lastMessage.avatar === MY_AVATAR) {
     lastMessage.lines.push(line)
@@ -12,4 +15,6 @@ export const addMessageToStore = (line) => {
       lines: [line]
     })
   }
+
+  store.isSyncing = false
 }
