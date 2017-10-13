87a26b2b692338d1e1029cf610521d4292d2a7a3
Glen Maddern <glenmaddern@gmail.com>
2017-10-13 11:11:27 +1100

    hit the classic this bug in React

--- src/App.js
+++ src/App.js
@@ -2,38 +2,36 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function a() {
-  return 5
-}
-
-const b = function () {
-  return 5
-}
-const c = () => {
-  console.log("hi!")
-  return 5
-}
-const d = () => 5
-
-console.log(a())
-console.log(b())
-console.log(c())
-console.log(d())
-
-const AppHeader = () => (
+const AppHeader = ({ greeting }) => (
   <header className="App-header">
     <img src={logo} className="App-logo" alt="logo"/>
-    <h1 className="App-title">Welcome to React</h1>
+    <h1 className="App-title">{ greeting }</h1>
   </header>
 )
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { greeting: "👻🕸 It's Friday 13th!" }
+  }
+  
+  handleButtonClick() {
+    this.setState({
+      greeting: "👻🕸 IT'S FRIDAY 13TH!"
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <AppHeader/>
+        <AppHeader greeting={this.state.greeting}/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          <button type="button" onClick={this.handleButtonClick}>
+            Click me!
+          </button>
         </p>
       </div>
     )
