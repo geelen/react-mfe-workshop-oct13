18c65c1f793f9ce6b5eaa82b2e4596ab93275eec
Glen Maddern <glenmaddern@gmail.com>
2017-10-13 13:28:20 +1100

    got a place to work on the chat app

---
+++ src/components/ChatApp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ChatApp = () => (
+  <h1>Chat app will live here.</h1>
+)
+
+export default ChatApp
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import ChatApp from './components/ChatApp'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ChatApp />,
+  document.getElementById('root')
+)
