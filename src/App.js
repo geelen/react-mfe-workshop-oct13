3b7d82d7590ee659963cc514872d80967083b45a
Glen Maddern <glenmaddern@gmail.com>
2017-10-13 11:04:22 +1100

    simple function shorthand explanation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+function a() {
+  return 5
+}
+
+const b = function () {
+  return 5
+}
+const c = () => {
+  console.log("hi!")
+  return 5
+}
+const d = () => 5
+
+console.log(a())
+console.log(b())
+console.log(c())
+console.log(d())
+
+const AppHeader = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo"/>
+    <h1 className="App-title">Welcome to React</h1>
+  </header>
+)
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <AppHeader/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
