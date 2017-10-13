0ac9a6bd88de222498fa91b7957048cd2407f44a
Glen Maddern <glenmaddern@gmail.com>
2017-10-13 13:32:24 +1100

    spammed out a bunch of placeholders to represent the structure

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -1,7 +1,37 @@
 import React from 'react'
 
+const MessageEntry = 'div'
+const Avatar = 'div'
+const Lines = 'div'
+const FillScreen = 'div'
+const ChatWindow = 'div'
+const InputArea = 'div'
+const InputField = 'div'
+const SubmitButton = 'div'
+
+const Message = () => (
+  <MessageEntry>
+    <Avatar/>
+    <Lines>
+      <div>Line one</div>
+      <div>Line two</div>
+      <div>Line three</div>
+    </Lines>
+  </MessageEntry>
+)
+
 const ChatApp = () => (
-  <h1>Chat app will live here.</h1>
+  <FillScreen>
+    <ChatWindow>
+      <Message/>
+      <Message/>
+      <Message/>
+    </ChatWindow>
+    <InputArea>
+      <InputField/>
+      <SubmitButton/>
+    </InputArea>
+  </FillScreen>
 )
 
 export default ChatApp
