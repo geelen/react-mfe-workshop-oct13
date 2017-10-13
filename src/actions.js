206a557b52d5fffd8047050459f3fa00b2d22203
Glen Maddern <glenmaddern@gmail.com>
2017-10-13 15:51:36 +1100

    calling a redux reducer from mobx actually works!

--- src/actions.js
+++ src/actions.js
@@ -1,16 +1,9 @@
 import store from './store'
-
-const MY_AVATAR = 'http://fillmurray.com/106/106'
+import { addMessageReducer } from './reducers'
 
 export const addMessageToStore = (line) => {
-  const lastMessage = store.messages[store.messages.length - 1]
-
-  if (lastMessage && lastMessage.avatar === MY_AVATAR) {
-    lastMessage.lines.push(line)
-  } else {
-    store.messages.push({
-      avatar: MY_AVATAR,
-      lines: [line]
-    })
-  }
+  store.messages = addMessageReducer(store, {
+    type: 'ADD_MESSAGE',
+    line
+  }).messages
 }
